d039fef5b52640d0fff62cdf237d7b1b2de0344e
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudAbuseeventLoggingV1/AbuseEvent.swift
+++ Sources/GoogleCloudAbuseeventLoggingV1/AbuseEvent.swift
@@ -213,7 +213,7 @@ public struct AbuseEvent: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:DetectionType/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "DETECTION_TYPE_UNSPECIFIED": self = .unspecified
@@ -229,7 +229,7 @@ public struct AbuseEvent: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:DetectionType/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
@@ -345,7 +345,7 @@ public struct AbuseEvent: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:ActionType/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "ACTION_TYPE_UNSPECIFIED": self = .unspecified
@@ -360,7 +360,7 @@ public struct AbuseEvent: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:ActionType/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
